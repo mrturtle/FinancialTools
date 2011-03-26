2731fc43f78cd03699ee028159cfaf9d283694fe
Evan Lucas <evanlucas@me.com>
2011-03-26 00:37:54 -0500

    modified:   FinancialTools.xcodeproj/project.pbxproj 	modified:   FinancialTools/FinancialToolsAppDelegate.h 	modified:   FinancialTools/FinancialToolsAppDelegate.m 	modified:   FinancialTools/en.lproj/MainMenu.xib

--- FinancialTools/FinancialToolsAppDelegate.h
+++ FinancialTools/FinancialToolsAppDelegate.h
@@ -9,10 +9,41 @@
 #import <Cocoa/Cocoa.h>
 
 @interface FinancialToolsAppDelegate : NSObject <NSApplicationDelegate> {
-@private
+
     NSWindow *window;
+    NSTextField *textN;
+    NSTextField *textIY;
+    NSTextField *textPV;
+    NSTextField *textPMT;
+    NSTextField *textFV;
+    NSTableView *amortizationTable;
+    NSButton *checkBoxIYAsPercentage;
+    NSButton *checkBoxNAsYears;
+    NSMutableArray *mArrayAmortizationTable;
+    NSUInteger decimalN;
+    NSDecimalNumber *decimalIY;
+    NSDecimalNumber *decimalPV;
+    NSDecimalNumber *decimalPMT;
+    NSDecimalNumber *decimalFV;
 }
 
 @property (assign) IBOutlet NSWindow *window;
+@property (nonatomic, retain) IBOutlet NSTextField *textN;
+@property (nonatomic, retain) IBOutlet NSTextField *textIY;
+@property (nonatomic, retain) IBOutlet NSTextField *textPV;
+@property (nonatomic, retain) IBOutlet NSTextField *textPMT;
+@property (nonatomic, retain) IBOutlet NSTextField *textFV;
+@property (nonatomic, retain) IBOutlet NSTableView *amortizationTable;
+@property (nonatomic, retain) IBOutlet NSButton *checkBoxIYAsPercentage;
+@property (nonatomic, retain) IBOutlet NSButton *checkBoxNAsYears;
+@property (nonatomic, retain) NSMutableArray *mArrayAmortizationTable;
+@property (nonatomic) NSUInteger decimalN;
+@property (nonatomic, retain) NSDecimalNumber *decimalIY;
+@property (nonatomic, retain) NSDecimalNumber *decimalPV;
+@property (nonatomic, retain) NSDecimalNumber *decimalPMT;
+@property (nonatomic, retain) NSDecimalNumber *decimalFV;
+- (IBAction)calculatePayment:(id)sender;
+- (IBAction)exportDataFromTable:(id)sender;
+- (IBAction)clearAllData:(id)sender;
 
 @end
--- FinancialTools/FinancialToolsAppDelegate.m
+++ FinancialTools/FinancialToolsAppDelegate.m
@@ -10,11 +10,106 @@
 
 @implementation FinancialToolsAppDelegate
 
-@synthesize window;
-
+@synthesize window, textN, textFV, textIY, textPV, textPMT, amortizationTable, checkBoxNAsYears, checkBoxIYAsPercentage, mArrayAmortizationTable;
+@synthesize decimalN, decimalFV, decimalIY, decimalPV, decimalPMT;
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
-    // Insert code here to initialize your application
+    
+}
+
+- (IBAction)calculatePayment:(id)sender {
+    if ([checkBoxNAsYears state] == NSOnState) {
+        decimalN = ([textN integerValue] * 12);
+    } else {
+        decimalN = [textN integerValue];
+    }
+    mArrayAmortizationTable = [[NSMutableArray alloc] init];
+    if ([checkBoxIYAsPercentage state] == NSOnState) {
+        decimalIY = [[NSDecimalNumber decimalNumberWithString:[textIY stringValue]] decimalNumberByDividingBy:[NSDecimalNumber decimalNumberWithString:@"100"]];
+    } else {
+        decimalIY = [NSDecimalNumber decimalNumberWithString:[textIY stringValue]];
+    }
+    decimalPV = [NSDecimalNumber decimalNumberWithString:[textPV stringValue]];
+    NSDecimalNumber *decimalR = [decimalIY decimalNumberByDividingBy:[NSDecimalNumber decimalNumberWithString:@"12"]];
+    NSDecimalNumber *decimalOnePlusRToTheN = [[[NSDecimalNumber decimalNumberWithString:@"1"] decimalNumberByAdding:decimalR] decimalNumberByRaisingToPower:decimalN];
+    NSDecimalNumber *numerator = [decimalR decimalNumberByMultiplyingBy:decimalOnePlusRToTheN];
+    NSDecimalNumber *denominator = [decimalOnePlusRToTheN decimalNumberBySubtracting:[NSDecimalNumber decimalNumberWithString:@"1"]];
+    NSDecimalNumber *full = [numerator decimalNumberByDividingBy:denominator];
+    NSDecimalNumber *decimalPayment = [decimalPV decimalNumberByMultiplyingBy:full];
+    NSDecimalNumber *decimalBeginningBalance;
+    NSDecimalNumber *decimalPrincipal;
+    NSDecimalNumber *decimalInterest; 
+    NSDecimalNumber *decimalEndingBalance;
+    
+    
+    decimalBeginningBalance = decimalPV;
+    decimalInterest = [decimalBeginningBalance decimalNumberByMultiplyingBy:decimalR];
+    decimalPrincipal = [decimalPayment decimalNumberBySubtracting:decimalInterest];
+    decimalEndingBalance = [decimalBeginningBalance decimalNumberBySubtracting:decimalPrincipal];
+    [mArrayAmortizationTable addObject:[NSDictionary dictionaryWithObjectsAndKeys:[NSString stringWithFormat:@"%d", 1], @"First",
+                                        decimalBeginningBalance, @"Second",
+                                        decimalPayment, @"Third",
+                                        decimalInterest, @"Fourth",
+                                        decimalPrincipal, @"Fifth",
+                                        decimalEndingBalance, @"Sixth", nil]];
+    
+    
+    int i;
+    for (i = 1; i < decimalN; i++) {
+        
+        decimalBeginningBalance = decimalEndingBalance;
+            decimalInterest = [decimalBeginningBalance decimalNumberByMultiplyingBy:decimalR];
+            decimalPrincipal = [decimalPayment decimalNumberBySubtracting:decimalInterest];
+            decimalEndingBalance = [decimalBeginningBalance decimalNumberBySubtracting:decimalPrincipal];
+            [mArrayAmortizationTable addObject:[NSDictionary dictionaryWithObjectsAndKeys:[NSString stringWithFormat:@"%d", i + 1], @"First",
+                                                decimalBeginningBalance, @"Second",
+                                                decimalPayment, @"Third",
+                                                decimalInterest, @"Fourth",
+                                                decimalPrincipal, @"Fifth",
+                                                decimalEndingBalance, @"Sixth", nil]];
+            
+        
+        
+    }
+    
+    [self.amortizationTable reloadData];
+    
+}
+
+- (IBAction)exportDataFromTable:(id)sender {
+}
+
+- (IBAction)clearAllData:(id)sender {
+    [mArrayAmortizationTable removeAllObjects];
+    [self.amortizationTable reloadData];
+}
+
+
+
+//   Table View methods
+
+- (NSInteger)numberOfRowsInTableView:(NSTableView *)tableView {
+    
+    return [mArrayAmortizationTable count];
+}
+
+- (id)tableView:(NSTableView *)tableView objectValueForTableColumn:(NSTableColumn *)tableColumn row:(NSInteger)row {
+    return [[mArrayAmortizationTable objectAtIndex:row] objectForKey:[tableColumn identifier]];
+    
+}
+
+- (void)dealloc {
+    [super dealloc];
+    [textN release];
+    [textIY release];
+    [textPV release];
+    [textPMT release];
+    [textFV release];
+    [amortizationTable release];
+    [checkBoxNAsYears release];
+    [checkBoxIYAsPercentage release];
+    [mArrayAmortizationTable release];
+    
 }
 
 @end
